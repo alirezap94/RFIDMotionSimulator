44e8d6953fdda16b25e807d37b67f444be83af0b
Alireza Pourafzal <69417176+alirezap94@users.noreply.github.com>
2023-12-04 14:37:53 +0100

    debugging

--- channel_RFID.m
+++ channel_RFID.m
@@ -16,12 +16,6 @@ function t = channel_RFID(t, PersonPresent)
 %% CONDITIONAL CHANNEL BEHAVIOR
 
 if PersonPresent
-    % If a person is present, apply random variations to tau and pdb
-    % tau modification: Adding small random noise (10^-10 scale) to elements 2 and 3 of tau array
-    t.tau(2:3) = t.tau(2:3) + 1e-10*randn(2,1);
-    % pdb modification: Adding standard Gaussian random noise to elements 2 and 3 of pdb array
-    t.pdb(2:3) = t.pdb(2:3) + randn(2,1);
-
     %%% Large-scale path loss calculation: 
     dist_flag = true;
     while dist_flag
@@ -30,7 +24,7 @@ if PersonPresent
         t.deltaDist = t.T_tot * (rand() - 0.5)*t.vmax; 
         t.DistanceNew = t.Distance + t.deltaDist; 
         % Ensuring the new distance is within a valid range (0 to 10 meters)
-        if (t.DistanceNew > 0) && (t.DistanceNew < 10)
+        if (t.DistanceNew > 0.5) && (t.DistanceNew < 10)
             dist_flag = false; 
         end
     end
@@ -40,27 +34,12 @@ if PersonPresent
     t.large_scale_coeff = 2.5;
     % Recalculating RSSI based on the new distance and large-scale path loss coefficient
     % RSSI reduction proportional to the logarithm of the ratio of new to old distance
-    t.RSSI = t.RSSIinit - 10 * t.large_scale_coeff * log10 (t.DistanceNew / t.Distance) ; 
+    t.RSSI = t.RSSIinit - 10 * t.large_scale_coeff * log10 (t.DistanceNew) ; 
     % Updating the distance to the new calculated value
     t.Distance = t.DistanceNew; 
-    
-    % Initialize RayleighChannel object with the person's presence affecting the channel
-    rayleighChan = comm.RayleighChannel( ...
-        'SampleRate', t.F_s, ...
-        'PathDelays', t.tau.', ...
-        'AveragePathGains', t.pdb.', ...
-        'MaximumDopplerShift', t.dopplermax, ...
-        'PathGainsOutputPort', true);
 else 
     % If no person is present, use the existing tau, pdb, and SNR without additional noise
     t.RSSI = t.RSSIinit; 
-    % Initialize RayleighChannel object without person's presence affecting the channel
-    rayleighChan = comm.RayleighChannel( ...
-        'SampleRate', t.F_s, ...
-        'PathDelays', t.tau.', ...
-        'AveragePathGains', t.pdb.', ...
-        'MaximumDopplerShift', 0, ...
-        'PathGainsOutputPort', true);
 end
 
 %% SIMULATE CHANNEL EFFECTS
@@ -70,7 +49,7 @@ t.NDelaySamples = randi([0, 100]);
 % Create received signal by appending zeros to manage delay
 Rx = [t.transmitted_signal; zeros(t.NDelaySamples,1)]; 
 % Use Rayleigh channel model to get the faded signal
-[t.RxFaded, ~] = rayleighChan(Rx);
+[t.RxFaded, ~] = t.rayleighChan(Rx);
 
 t.RxLargeScale = sqrt(10^(t.RSSI/10)) *t.RxFaded; 
 
